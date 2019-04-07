81b9b5de0c5a5905dd6b8f078190d50f8d7fd461
Adam Gomulka <adam.gomulka4@gmail.com>
2019-04-06 22:38:59 -0700

    correct pointer reference and add healthcheck structure

--- main.go
+++ main.go
@@ -8,11 +8,11 @@ import (
     "encoding/json"
 )
 
-type counterHandler struct {
+type CounterHandler struct {
     counts map[string]int
 }
 
-func (c counterHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+func (c *CounterHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
     if strings.HasPrefix(r.URL.Path[1:], "hello") {
         c.ServeHello(r.URL.Path[len("/hello/:"):], w)
     } else if strings.HasPrefix(r.URL.Path[1:], "counts") {
@@ -24,11 +24,11 @@ func (c counterHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
             http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
         }
     } else {
-        http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+         http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
     }
 }
 
-func (c *counterHandler) ServeHello(n string, w http.ResponseWriter) {
+func (c *CounterHandler) ServeHello(n string, w http.ResponseWriter) {
     if _, p := c.counts[n]; p {
         c.counts[n]++
     } else {
@@ -37,22 +37,27 @@ func (c *counterHandler) ServeHello(n string, w http.ResponseWriter) {
     fmt.Fprintf(w, "Hello, %s!", n)
 }
 
-func (c *counterHandler) GetCounter(w http.ResponseWriter) {
+func (c *CounterHandler) GetCounter(w http.ResponseWriter) {
     j, err := json.Marshal(c.counts)
     if err == nil {
         w.Write(j)
     }
 }
 
-func (c *counterHandler) ResetCounter() {
+func (c *CounterHandler) ResetCounter() {
     c.counts = make(map[string]int)
+}
+
+func HealthCheck() {
+    var health map[string]int
+
 }
 
 func main() {
     server := http.NewServeMux()
-    var counter counterHandler
-    server.Handle("/hello/:", counter)
+    counter := &CounterHandler{counts: map[string]int{}}
+    server.Handle("/hello/", counter)
     server.Handle("/counts", counter)
-    log.Fatal(http.ListenAndServe(":80", server))
+    log.Fatal(http.ListenAndServe(":8080", server))
 }
 
